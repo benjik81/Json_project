1aa1e415ce48fd88b3f4faf9b3a8aaee633d6dc9
Benjik <ben12pr@hotmail.com>
2022-05-07 22:10:59 +0200

    {Add} Player.cs

---
+++ json_project_unity/Assets/Scripts/Save/Player.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Player
+{
+    public string name;
+    public bool allElementsInScene = false;
+    public int count = 0;
+}
