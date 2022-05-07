e21b5d2a1660de3d66d93e97469da79373307574
Benjik <ben12pr@hotmail.com>
2022-05-07 22:10:36 +0200

    {Add} Donnees.cs

---
+++ json_project_unity/Assets/Scripts/Save/Donnees.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Donnees
+{
+    public Dictionary<string, Player> joueurs = new Dictionary<string, Player>();
+}
