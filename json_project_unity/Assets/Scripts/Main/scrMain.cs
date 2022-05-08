9a56eab5d93bc60f07afaf772c5c26d7fc828af9
Benjik <ben12pr@hotmail.com>
2022-05-08 20:31:29 +0200

    {Update} scrMain.cs

--- json_project_unity/Assets/Scripts/Main/scrMain.cs
+++ json_project_unity/Assets/Scripts/Main/scrMain.cs
@@ -10,6 +10,7 @@ public class scrMain : MonoBehaviour
 {
     [Header("Json")]
     public GameObject JSON_reader;
+    public GameObject JSON_backgrounds_reader;
 
     [Header("Username zone")]
     [SerializeField] private TMP_InputField userName;
@@ -25,10 +26,24 @@ public class scrMain : MonoBehaviour
     public GameObject endGameScene;
     public GameObject gameScene;
     public GameObject menuScene;
+
+    [Header("Backgrounds")]
+    public GameObject connexionBackground;
+    public GameObject gameBackground;
+    public GameObject menuBackground;
+    public GameObject endBackground;
+
+    [Header("Buttons Backgrounds")]
+    public GameObject exitButton;
+    public GameObject validateButton;
+    public GameObject playButton;
+    public GameObject exitMenuButton;
+    public GameObject backButton;
+    public GameObject endGameExitButton;
     
     private bool isValid = false;
     private bool isLoad = false;
-    private Donnees data;
+    private PlayerDico data;
     private Player player;
 
     void Start()
@@ -39,16 +54,23 @@ public class scrMain : MonoBehaviour
         gameData.allElementsInScene = false;
         gameData.nbErrors = 0;
         isValid = false;
-        for(int i = 0; i < limit; i++)
+        for (int i = 0; i < limit; i++)
         {
             if(JSON_reader.GetComponent<JSON_reader>().GetType(i) == "Slot")
             {
                 createElements(slotPrefab, "Slots", i);
             }
-            else
+            if(JSON_reader.GetComponent<JSON_reader>().GetType(i) == "Item")
             {
                 createElements(itemPrefab, "Items", i);
             }
+        }
+
+        int limit_backgrounds = JSON_backgrounds_reader.GetComponent<JSON_background_Reader>().backgroundsList.backgrounds.Length;
+        Debug.Log("Nombre de PNG : " + limit_backgrounds);
+        for(int j = 0; j < limit_backgrounds; j++)
+        {
+            CreateBackgrounds(j);
         }
     }
 
@@ -59,13 +81,66 @@ public class scrMain : MonoBehaviour
         go.transform.SetParent(GameObject.Find(name).transform);
         go.GetComponent<RectTransform>().anchoredPosition = new Vector2(JSON_reader.GetComponent<JSON_reader>().GetPosition(indice).x, JSON_reader.GetComponent<JSON_reader>().GetPosition(indice).y);
         go.GetComponent<RectTransform>().sizeDelta = new Vector2(JSON_reader.GetComponent<JSON_reader>().GetDimension(indice).width, JSON_reader.GetComponent<JSON_reader>().GetDimension(indice).height);
-        go.GetComponent<Image>().sprite = Resources.Load<Sprite>(JSON_reader.GetComponent<JSON_reader>().GetSourceImage(indice));
+        go.GetComponent<Image>().sprite = CreateSprite(indice);
+        //Resources.Load<Sprite>(JSON_reader.GetComponent<JSON_reader>().GetSourceImage(indice));
         go.name = JSON_reader.GetComponent<JSON_reader>().GetName(indice);
         go.GetComponent<RectTransform>().rotation = Quaternion.Euler(
             JSON_reader.GetComponent<JSON_reader>().GetRotation(indice).rX,
             JSON_reader.GetComponent<JSON_reader>().GetRotation(indice).rY,
             JSON_reader.GetComponent<JSON_reader>().GetRotation(indice).rZ
             );
+    }
+
+    public void CreateBackgrounds(int indice)
+    {
+        Debug.Log("TYPE : " + JSON_backgrounds_reader.GetComponent<JSON_background_Reader>().backgroundsList.backgrounds[indice].type);
+        switch (JSON_backgrounds_reader.GetComponent<JSON_background_Reader>().backgroundsList.backgrounds[indice].type)
+        {
+            case "connexion_background":
+                Debug.Log("CONNEXION BACKGROUND");
+                connexionBackground.GetComponent<Image>().sprite = CreateBackgroundSprite(indice, connexionBackground);
+                break;
+            case "buttons_background":
+                exitButton.GetComponent<Image>().sprite = CreateBackgroundSprite(indice, exitButton);
+                validateButton.GetComponent<Image>().sprite = CreateBackgroundSprite(indice, validateButton);
+                playButton.GetComponent<Image>().sprite = CreateBackgroundSprite(indice, playButton);
+                exitMenuButton.GetComponent<Image>().sprite = CreateBackgroundSprite(indice, exitMenuButton);
+                endGameExitButton.GetComponent<Image>().sprite = CreateBackgroundSprite(indice, endGameExitButton);
+                break;
+            case "all_background":
+                menuBackground.GetComponent<Image>().sprite = CreateBackgroundSprite(indice, menuBackground);
+                gameBackground.GetComponent<Image>().sprite = CreateBackgroundSprite(indice, gameBackground);
+                break;
+            case "end_background":
+                endBackground.GetComponent<Image>().sprite = CreateBackgroundSprite(indice, endBackground);
+                break;
+            case "back_background":
+                backButton.GetComponent<Image>().sprite = CreateBackgroundSprite(indice, backButton);
+                break;
+        }
+    }
+
+    public Sprite CreateBackgroundSprite(int indice, GameObject background)
+    {
+        float width = background.GetComponent<RectTransform>().sizeDelta.x;
+        float height = background.GetComponent<RectTransform>().sizeDelta.y;
+        string pathImage = Application.streamingAssetsPath + "/" + JSON_backgrounds_reader.GetComponent<JSON_background_Reader>().backgroundsList.backgrounds[indice].imagesource + ".png";
+        byte[] pngBytes = System.IO.File.ReadAllBytes(pathImage);
+        Texture2D tex = new Texture2D((int)width, (int)height);
+        tex.LoadImage(pngBytes);
+        return Sprite.Create(tex, new Rect(0, 0, tex.width, tex.height), new Vector2(1f, 1f));
+    }
+
+    public Sprite CreateSprite(int indice)
+    {
+        int width = JSON_reader.GetComponent<JSON_reader>().GetDimension(indice).width;
+        int height = JSON_reader.GetComponent<JSON_reader>().GetDimension(indice).height;
+        string pathImage = Application.streamingAssetsPath + "/" + JSON_reader.GetComponent<JSON_reader>().GetSourceImage(indice) + ".png";
+        byte[] pngBytes = System.IO.File.ReadAllBytes(pathImage);
+        Texture2D tex = new Texture2D(width, height);
+        tex.LoadImage(pngBytes);
+        Sprite result = Sprite.Create(tex, new Rect(0, 0, tex.width, tex.height), new Vector2(1f, 1f));
+        return result;
     }
 
     void Update()
@@ -152,7 +227,14 @@ public class scrMain : MonoBehaviour
         player = GetPlayer();
         Debug.Log(player.name + player.allElementsInScene + player.count);
         gameData.allElementsInScene = player.allElementsInScene;
-        gameData.count = player.count;
+        if(player.count != JSON_reader.GetComponent<JSON_reader>().donneesList.donnees.Length/2 && player.count != 0)
+        {
+            gameData.count = 0;
+        }
+        else
+        {
+            gameData.count = player.count;
+        }
         gameData.playerName = player.name;
         gameData.nbErrors = player.nbErrors;
     }
@@ -180,7 +262,7 @@ public class scrMain : MonoBehaviour
         string jsonFile = File.ReadAllText(path);
         Debug.Log("JsonFile : " + jsonFile);
         //gameData = JsonConvert.DeserializeObject<GameDataScript>(jsonFile);
-        data = JsonConvert.DeserializeObject<Donnees>(jsonFile);
+        data = JsonConvert.DeserializeObject<PlayerDico>(jsonFile);
         displayData(data);
     }
 
@@ -194,7 +276,7 @@ public class scrMain : MonoBehaviour
         userName.text = "";
     }
 
-    public void displayData(Donnees d)
+    public void displayData(PlayerDico d)
     {
         foreach(KeyValuePair<string, Player> kvp in d.joueurs)
         {
