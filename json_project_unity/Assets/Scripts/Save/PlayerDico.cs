d6d5f4d814e47161b9e1e1e4cdd4ae3571d4288a
Benjik <ben12pr@hotmail.com>
2022-05-08 20:32:31 +0200

    {Add} PlayerDico.cs

---
+++ json_project_unity/Assets/Scripts/Save/PlayerDico.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class PlayerDico
+{
+    public Dictionary<string, Player> joueurs = new Dictionary<string, Player>();
+}
