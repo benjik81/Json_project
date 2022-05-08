d7fb1a86fb93d8c9759a40fd47c619a77ea6c3e9
Benjik <ben12pr@hotmail.com>
2022-05-08 10:28:00 +0200

    {Update} GameDataScript.cs

--- json_project_unity/Assets/Scripts/Datas/GameDataScript.cs
+++ json_project_unity/Assets/Scripts/Datas/GameDataScript.cs
@@ -8,4 +8,5 @@ public class GameDataScript : ScriptableObject
     public bool allElementsInScene = false;
     public int count = 0;
     public string playerName = "";
+    public int nbErrors = 0;
 }
